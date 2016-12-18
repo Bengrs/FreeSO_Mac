e7848bd9783849203c7008731c7326eafbaeb922
Bengrs <gross.benoit@gmail.com>
2016-12-18 22:13:50 +0100

    Initial commit

---
+++ FreeSO_Mac/Main.cs
@@ -0,0 +1,23 @@
+﻿#region Using Statements
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using AppKit;
+using Foundation;
+#endregion
+
+namespace FreeSO_Mac
+{
+	static class Program
+	{
+		/// <summary>
+		/// The main entry point for the application.
+		/// </summary>
+		static void Main(string[] args)
+		{
+			NSApplication.Init();
+			FSO.Client.Program.Main(args);
+		}
+	}
+}
